fb227fe85941ed2d8770cae8a3a1237866dca97e
James Brink <brink.james@gmail.com>
2018-06-15 03:04:09 -0700

    Updated arch_linux.sh script to use temp directory

--- arch_linux.sh
+++ arch_linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Variables
-SRC=./src
+SRC=$(mktemp -d)
 DEST=/
 REPO_URL="https://archive.archlinux.org/packages/l/linux-headers"
 VERSION=$(uname -r|sed 's/\-ARCH//g')
@@ -10,14 +10,17 @@ PACKAGE_NAME=linux-headers-$VERSION-$ARCH
 FILE_NAME=$PACKAGE_NAME.pkg.tar.xz
 
 # Download and extract package.
-echo "Downloading Arch linux package: $FILE_NAME"
+echo "Downloading Arch linux package: $PACKAGE_NAME"
+echo "Saving file to: $SRC/$FILE_NAME"
 curl "$REPO_URL/$FILE_NAME" --output "$SRC/$FILE_NAME"
+echo "Decompressing: $SRC/$FILE_NAME"
 xz -fd "$SRC/$FILE_NAME"
 mkdir -p $DEST
-tar xf "$SRC/$PACKAGE_NAME.pkg.tar" -C $DEST
+echo "Extracting: $SRC/$PACKAGE_NAME.pkg.tar => $DEST"
+tar xf "$SRC/$PACKAGE_NAME.pkg.tar" --exclude=.PKGINFO --exclude=.MTREE --exclude=.BUILDINFO -C $DEST
 
 # Cleanup
+echo "Removing: $SRC/$PACKAGE_NAME.pkg.tar"
 rm "$SRC/$PACKAGE_NAME.pkg.tar"
-rm "$DEST/.PKGINFO"
-rm "$DEST/.MTREE"
-rm "$DEST/.BUILDINFO"
+echo "Removing directory: $SRC"
+rmdir "$SRC"
